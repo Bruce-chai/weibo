b20a2f63ec9b9478d089d00be4ed188855191aa5
chaishuai <a_good_start163@163.com>
2020-03-23 14:53:44 +0000

    查看用户列表

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $users = factory(User::class)->times(50)->make();
+        User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+
+        $user = User::find(1);
+        $user->name = 'chaishuai';
+        $user->email = 'a_good_start163@163.com';
+        $user->save();
+    }
+}
