ac90e6cbfa0ddad55437d16a6fc85ec5cfb543f8
chaishuai <a_good_start163@163.com>
2020-03-23 13:26:59 +0000

    更新个人资料

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -10,9 +10,9 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
-          <a class="dropdown-item" href="#">编辑资料</a>
+          <a class="dropdown-item" href="{{ route('users.edit', Auth::user()) }}">编辑资料</a>
           <div class="dropdown-divider"></div>
-          <a class="dropdown-item" id="logout" href="#">
+          <a class="dropdown-item" id="logout" href="{{ route('users.destroy', Auth::user()) }}">
             <form action="{{ route('logout') }}" method="POST">
               {{ csrf_field() }}
               {{ method_field('DELETE') }}
