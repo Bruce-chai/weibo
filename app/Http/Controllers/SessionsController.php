85e2de09048f0ed69193611e960742312f9de3d1
chaishuai <a_good_start163@163.com>
2020-03-23 16:22:24 +0000

    注册邮箱激活

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,9 +27,16 @@ class SessionsController extends Controller
         ]);
 
         if (Auth::attempt($credentials, $request->has('remember'))) {
-            session()->flash('success', '欢迎回来');
-            $fallBack = route('users.show', Auth::user());
-            return redirect()->intended($fallBack);
+            if (Auth::user()->activated) {
+                session()->flash('success', '欢迎回来');
+                $fallBack = route('users.show', Auth::user());
+                return redirect()->intended($fallBack);
+            } else {
+                Auth::logout();
+                session()->flash('warning', '您的账号未激活，请检查邮箱中的注册邮件进行激活');
+                return redirect('/');
+            }
+
         } else {
             session()->flash('danger', '很抱歉，你的邮箱和密码不匹配');
             return redirect()->back()->withInput();
